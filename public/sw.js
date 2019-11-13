41258fa9b0e2fa5c4b90923f721de621757391bd
dusan.vesic <dusan.vesic@gmail.com>
2019-11-13 10:35:35 +0100

    Intercept styles request

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,29 @@
-self.addEventListener("install", function(event) {
-  console.log("install");
+importScripts("/localforage.js");
+
+const color = "papayawhip";
+const key = 'color';
+
+self.addEventListener("install", async function(event) {
+  await localforage.setItem(key, color);
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", async function(event) {
   console.log("activate");
 });
 
 self.addEventListener("fetch", function(event) {
-  return fetch(event.request);
+  if (event.request.url.includes("styles.css")) {
+    event.respondWith(
+      // intercept styles request
+      // and generate custom response
+      // including color stored in indexDB
+      (async function() {
+        let color = await localforage.getItem(key);
+        return new Response(
+          `body {background: ${color}!important;}`, 
+          {headers: { "Content-Type": "text/css" }
+        });
+      })()
+    );
+  }
 });
