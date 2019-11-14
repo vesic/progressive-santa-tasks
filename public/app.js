a107622dcf1d87044ef7419cea60342b299547e1
dusan.vesic <dusan.vesic@gmail.com>
2019-11-14 13:45:57 +0100

    Show install prompt

--- public/app.js
+++ public/app.js
@@ -1,7 +1,33 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js").then(function(registration) {
-    console.log("registration", registration.scope);
-  }, function(error) {
-    console.log("error", error);
-  });
+  navigator.serviceWorker.register("sw.js").then(
+    function(registration) {
+      console.log("registration", registration.scope);
+    },
+    function(error) {
+      console.log("error", error);
+    }
+  );
 }
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", e => {
+  deferredPrompt = e;
+});
+
+document.querySelector("button").addEventListener("click", e => {
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then(choiceResult => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the install prompt");
+    } else {
+      console.log("User dismissed the install prompt");
+    }
+    deferredPrompt = null;
+  });
+});
+
+// Extra points for statistics
+window.addEventListener("appinstalled", evt => {
+  console.log("App installed");
+});
