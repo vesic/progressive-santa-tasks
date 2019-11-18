4371be11b8302a5977cccadba78ae577f0ae85a2
dusan.vesic <dusan.vesic@gmail.com>
2019-11-18 11:53:38 +0100

    For jpeg request serve equivalent webp image

--- public/sw.js
+++ public/sw.js
@@ -7,5 +7,22 @@ self.addEventListener("activate", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
+  // if a request is an image
+  if (/\.jpg$|.jpeg$|.png$/.test(event.request.url)) {
+    var isSupported = false;
+    if (event.request.headers.has("accept")) {
+      // test if browser supports webp
+      // i.e. if it sends accept webp headers
+      isSupported = event.request.headers
+        .get('accept')
+        .includes('webp');
+    }
+    if (isSupported) {
+      // req is an stream - it can be consumed only once
+      var req = event.request.clone();
+      var returnUrl = req.url.substr(0, req.url.lastIndexOf(".")) + ".webp";
+      event.respondWith(fetch(returnUrl));
+    }
+  } 
   return fetch(event.request);
 });
