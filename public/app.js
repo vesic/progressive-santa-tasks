e97fa7df57dff8e75aa7eb8fc9bc3ba1be79605e
dusan.vesic <dusan.vesic@gmail.com>
2019-11-18 14:08:01 +0100

    Listen to push msg and determine which action was choosen by the user

--- public/app.js
+++ public/app.js
@@ -1,7 +1,16 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js").then(function(registration) {
-    console.log("registration", registration.scope);
-  }, function(error) {
-    console.log("error", error);
+window.addEventListener("load", function(event) {
+  Notification.requestPermission().then(function(permission) {
+    console.log("permission", permission);
   });
+});
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js").then(
+    function(registration) {
+      console.log("registration", registration.scope);
+    },
+    function(error) {
+      console.log("error", error);
+    }
+  );
 }
--- public/sw.js
+++ public/sw.js
@@ -9,3 +9,36 @@ self.addEventListener("activate", function(event) {
 self.addEventListener("fetch", function(event) {
   return fetch(event.request);
 });
+
+self.addEventListener("push", function(event) {
+  event.waitUntil(
+    registration.showNotification("hello", {
+      actions: [
+        { action: "foo", title: "Foo" },
+        { action: "bar", title: "Bar" }
+      ]
+    })
+  );
+});
+
+self.addEventListener("notificationclick", function(event) {
+  if (event.action === "foo") {
+    // we can
+    // clients.openWindow("http://localhost:3000/");
+    // or client focus
+    foo();
+  } else {
+    // clients.openWindow("http://localhost:3000/");
+    bar();
+  }
+});
+
+function foo() {
+  // do something here
+  console.log('foo');
+}
+
+function bar() {
+  // do something here
+  console.log('bar');
+}
